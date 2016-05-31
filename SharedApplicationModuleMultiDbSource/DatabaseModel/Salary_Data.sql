3cbb93265a3a358207a49cf8fcfeab8252a717f4
Markus Klenke <mke@team-pb.de>
2016-05-31 23:44:30 +0200

    initial commit

---
+++ SharedApplicationModuleMultiDbSource/DatabaseModel/Salary_Data.sql
@@ -0,0 +1,6 @@
+Insert into SALARY_STEPS (ID,SALARY,DESCRIPTION) values (1,1000,'Minimal');
+Insert into SALARY_STEPS (ID,SALARY,DESCRIPTION) values (2,2000,'Second to minimal');
+Insert into SALARY_STEPS (ID,SALARY,DESCRIPTION) values (3,5000,'Regular');
+Insert into SALARY_STEPS (ID,SALARY,DESCRIPTION) values (4,7500,'Advanced');
+Insert into SALARY_STEPS (ID,SALARY,DESCRIPTION) values (5,12500,'High');
+Insert into SALARY_STEPS (ID,SALARY,DESCRIPTION) values (6,17500,'Superb');
